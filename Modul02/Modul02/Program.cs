dde0367671ded7f80d86783aa95fe62af38e158b
Мартин Хаджиев <martinhdajiev@gmail.com>
2016-06-16 17:56:30 +0300

    Модул 02, Видео 01 - Добавяне на коментари

---
+++ Modul02/Modul02/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Modul02
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+
+			//дефиниране на променливи
+
+			//събиране на числа
+
+			//писане в конзолата
+		}
+	}
+}
